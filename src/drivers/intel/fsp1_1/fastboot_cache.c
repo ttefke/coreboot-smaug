25c53346c58ce77c5710273bedbca6a8a1cf143f
Lee Leahy <leroy.p.leahy@intel.com>
2015-07-08 15:52:48 -0700

    FSP: Fix the copyright notices
    Update the copyright notices to match what is in coreboot.org.
    
    BRANCH=none
    BUG=None
    TEST=Build and run on Braswell and Skylake
    
    Change-Id: Iab49cd2637948cc4b31d2a31eef5103bdde57be3
    Signed-off-by: Lee Leahy <Leroy.P.Leahy@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/284279
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Leroy P Leahy <leroy.p.leahy@intel.com>
    Tested-by: Leroy P Leahy <leroy.p.leahy@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/285248
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    Trybot-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>

--- src/drivers/intel/fsp1_1/fastboot_cache.c
+++ src/drivers/intel/fsp1_1/fastboot_cache.c
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2012 Google Inc.
  * Copyright (C) 2013-2014 Sage Electronic Engineering, LLC.
+ * Copyright (C) 2015 Intel Corp.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +16,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ * Foundation, Inc.
  */
 
 #include <stdint.h>
